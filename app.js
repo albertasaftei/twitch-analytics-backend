95bea3eb6cb9c4618708f989cd75896814ff2c81
alburt <albert.asaftei00@gmail.com>
2024-04-30 00:22:25 +0200

    🛂 ✨ feat: added authentication layer to the api

--- app.js
+++ app.js
@@ -9,11 +9,41 @@ import { fileURLToPath } from "url";
 
 const app = express();
 
-// view engine setup
+function authentication(req, res, next) {
+  const authheader = req.headers.authorization;
+  console.log(req.headers);
+
+  if (!authheader) {
+    let err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+
+  const auth = new Buffer.from(authheader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  const user = auth[0];
+  const pass = auth[1];
+
+  if (
+    user == process.env.CRONJOB_USER &&
+    pass == process.env.CRONJOB_PASSWORD
+  ) {
+    // If Authorized user
+    next();
+  } else {
+    let err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(authentication);
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
